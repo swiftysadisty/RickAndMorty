2962de257fcddd4a1c6aca8d843291f3c4c73767
Alexey <swiftslayeer@gmail.com>
2024-05-09 04:46:17 +0300

    Version 0.0

---
+++ RickAndMorty/Controller/MainController.swift
@@ -0,0 +1,13 @@
+import SnapKit
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
