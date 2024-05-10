a91350b4927f1e8d798ca95ef6e218e8e7016d9f
Alexey <swiftslayeer@gmail.com>
2024-05-10 21:32:47 +0300

    Добавил UINavigation

--- RickAndMorty/Controller/MainController.swift
+++ RickAndMorty/Controller/MainController.swift
@@ -5,9 +5,19 @@ class MainController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigation()
         view.backgroundColor = .red
     }
 
-
+    private func setupNavigation() {
+        let titleNavigaton = UILabel()
+        titleNavigaton.text = "Characters"
+        titleNavigaton.font = UIFont(name: "Gilroy-Bold", size: 28)
+        titleNavigaton.textColor = .white
+        
+        let leftItem = UIBarButtonItem(customView: titleNavigaton)
+        self.navigationItem.leftBarButtonItem = leftItem
+    }
 }
 
+#Preview{MainController()}
